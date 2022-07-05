63bf6a42cf57790ac4004a21bcc183029756d712
yogurt-mr <105791710+yogurt-mr@users.noreply.github.com>
2022-07-06 01:03:57 +0900

    Issue4

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validates :encrypted_password, presence: true,length: {minimum: 6}
+  # validates :nickname, presence: true
+  # validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name_kana, presence: true
+  # validates :first_name_kana, presence: true
+  # validates :dob, presence: true
+
 end
--- db/schema_bk.rb
+++ db/schema_bk.rb
@@ -10,17 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_120254) do
+ActiveRecord::Schema.define(version: 2022_07_05_034718) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nickname", default: "", null: false
-    t.string "last_name", default: "", null: false
-    t.string "first_name", default: "", null: false
-    t.string "last_name_kana", default: "", null: false
-    t.string "first_name_kana", default: "", null: false
-    t.datetime "dob", default: "2022-07-05 12:08:51", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
