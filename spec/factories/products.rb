8b98acf616d57ec94ed60adfa77a2b537a863004
yogurt-mr <105791710+yogurt-mr@users.noreply.github.com>
2022-07-08 01:14:13 +0900

    テスト完了

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :product do
+    id            {'1'}
+    product_name          {'ああ'}
+    description           {'ああああ'}
+    category_id           {'2'}
+    product_status_id     {'2'}
+    shipping_fee_id       {'2'}
+    shipping_area_id      {'2'}
+    shipping_day_id       {'2'}
+    price                 {'10000'}
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/product_spec_bk.rb
+++ spec/models/product_spec_bk.rb
@@ -4,8 +4,6 @@ RSpec.describe Product, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   before do
     @product = FactoryBot.build(:product)
-
-    # binding.pry
   end
 
   describe '商品出品' do
@@ -33,19 +31,19 @@ RSpec.describe Product, type: :model do
         it 'category_idが空では登録されない' do
         @product.category_id = nil
         @product.valid?
-        expect(@product.errors.full_messages).to include("Category can't be blank")
+        expect(@product.errors.full_messages).to include("Category can't be blank", "Category is not a number")
       end
         it 'product_status_idが空では登録されない' do
         @product.product_status_id = nil
         @product.valid?
         expect(@product.errors.full_messages).to include("Product status can't be blank")
       end
-        it 'shipping_fee_idが空では登録されない' do
+        it 'shipping_fee_status_idが空では登録されない' do
         @product.shipping_fee_id = nil
         @product.valid?
         expect(@product.errors.full_messages).to include("Shipping fee can't be blank")
       end
-      it 'shipping_area_idが空では登録されない' do
+      it 'prefecture_idが空では登録されない' do
         @product.shipping_area_id = nil
         @product.valid?
         expect(@product.errors.full_messages).to include("Shipping area can't be blank")
@@ -59,15 +57,15 @@ RSpec.describe Product, type: :model do
         @product.category_id = '1'
         @product.product_status_id = '1'
         @product.shipping_fee_id = '1' 
-        @product.shipping_area_id = '1'
+        @product.prefecture_id = '1'
         @product.shipping_day_id = '1'
         @product.valid?
-        expect(@product.errors.full_messages).to include("Category can't be blank", "Product status can't be blank", "Shipping fee can't be blank", "Shipping area can't be blank", "Shipping day can't be blank")
+        expect(@product.errors.full_messages).to include("Category must be other than 1", "Product status must be other than 1", "Shipping fee must be other than 1", "Shipping area must be other than 1", "Shipping day must be other than 1")
       end
       it 'priceが空では登録されない' do
         @product.price = ""
         @product.valid?
-        expect(@product.errors.full_messages).to include("Price is invalid", "Price is not a number")
+        expect(@product.errors.full_messages).to include("Price can't be blank", "Price is invalid", "Price is not a number")
       end
       it 'priceが半角数字以外では登録できない' do
         @product.price = "１０００"
