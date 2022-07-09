154ae0c86c39bf9f67fb1348a7c79a45647c291a
yogurt-mr <105791710+yogurt-mr@users.noreply.github.com>
2022-07-09 17:28:06 +0900

    指摘事項反映

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,8 +40,8 @@ class ProductsController < ApplicationController
   def destroy
     if user_signed_in? && current_user.id == @product.user_id
       @product.destroy
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 
   private
