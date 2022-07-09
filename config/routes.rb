97645f7a399eed124fe6bec7d70ea35b9fbdc045
yogurt-mr <105791710+yogurt-mr@users.noreply.github.com>
2022-07-09 16:08:47 +0900

    商品編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "products#index"
   resources :users, only: :show
-  resources :products, only: [:index, :new,:create,:show]
+  resources :products, only: [:index,:new,:create,:show,:edit, :update]
 end
