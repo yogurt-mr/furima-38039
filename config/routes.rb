5be8b828610553209fb89c643108878d44873c28
yogurt-mr <105791710+yogurt-mr@users.noreply.github.com>
2022-07-09 17:12:09 +0900

    削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "products#index"
   resources :users, only: :show
-  resources :products, only: [:index,:new,:create,:show,:edit, :update]
+  resources :products, only: [:index,:new,:create,:show,:edit,:update,:destroy]
 end
